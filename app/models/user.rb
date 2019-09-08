0aee022f51e3884f64d09fa32753400381a2e664
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-08 13:21:57 +0000

    user validate on upadate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
 before_save { self.email.downcase! }
+
   validates :name, presence: true, length: { maximum: 50 }
+  
   validates :email, presence: true, length: { maximum: 255},
    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
+                    
+  validates :age, presence: true, length: { maximum: 255}, on: :update
+  validates :gender, presence: true, length: { maximum: 255}, on: :update
+  validates :area, presence: true, length: { maximum: 255}, on: :update
+  validates :part, presence: true, length: { maximum: 255}, on: :update
+  validates :category, presence: true, length: { maximum: 255}, on: :update
+  validates :sns_1, presence: true, length: { maximum: 255}, on: :update
+  validates :sns_2, presence: true, length: { maximum: 255}, on: :update
+  validates :icon, presence: true, length: { maximum: 255}, on: :update
   
-  validates :age, presence: true, length: { maximum: 255}
-  validates :gender, presence: true, length: { maximum: 255}
-  validates :area, presence: true, length: { maximum: 255}
-  validates :part, presence: true, length: { maximum: 255}
-  validates :category, presence: true, length: { maximum: 255}
-  validates :sns_1, presence: true, length: { maximum: 255}
-  validates :sns_2, presence: true, length: { maximum: 255}
-  validates :icon, presence: true, length: { maximum: 255}
+  has_secure_password
 end
