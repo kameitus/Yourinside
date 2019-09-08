633eea69415dd646e34acca1fd792b0733dc5277
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-08 12:11:31 +0000

    User model validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255},
+   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  
+  validates :age, presence: true, length: { maximum: 255}
+  validates :gender, presence: true, length: { maximum: 255}
+  validates :area, presence: true, length: { maximum: 255}
+  validates :part, presence: true, length: { maximum: 255}
+  validates :category, presence: true, length: { maximum: 255}
+  validates :sns_1, presence: true, length: { maximum: 255}
+  validates :sns_2, presence: true, length: { maximum: 255}
+  validates :icon, presence: true, length: { maximum: 255}
 end
