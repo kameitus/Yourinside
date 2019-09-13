d757aaf7616e412e283ba4ffd232939032f3b84d
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-13 13:31:46 +0000

    gem install rmagick

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -1,6 +1,6 @@
 class IconUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -14,12 +14,14 @@ class IconUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+   def default_url(*args)
+      #For Rails 3.1+ asset pipeline compatibility:
+      #ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+   end
+
+  process :resize_to_limit => [640, 480]
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -29,9 +31,9 @@ class IconUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+  process resize_to_fit: [640, 480]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
