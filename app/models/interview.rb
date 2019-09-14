234b9542275c7357e34b9cb59e392f2931d4a726
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-14 14:45:54 +0000

    validate

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,14 @@
 class Interview < ApplicationRecord
   belongs_to :user
+  
+  validates :answer_1, presence: true
+  validates :answer_2, presence: true
+  validates :answer_3, presence: true
+  validates :answer_4, presence: true
+  validates :answer_5, presence: true
+  validates :answer_6, presence: true
+  validates :answer_7, presence: true
+  validates :answer_8, presence: true
+  validates :answer_9, presence: true
+  validates :title, presence: true,length: { maximum: 30 }
 end
