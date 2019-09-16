17ba59320c7d4eadde378ce3e025a3df48862014
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-16 12:54:13 +0000

    intervie入力ページ　テキスト部分完了

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -3,26 +3,30 @@ before_action :require_user_logged_in, only:[:new,:create,:update,:destroy]
   
 #検索＆検索結果表示ページ
   def index
-    @interviews = current_user.interviews.order(id: :desc).page(params[:page])
+    @interviews = current_user.interview.order(id: :desc).page(params[:page])
   end
   
 #公開インタビューページ（閲覧はログイン不要）
   def show
+    @interview = Interview.find(params[:id])
+    @user = current_user
   end
   
   def new
+    @interview = Interview.new
     @user = current_user
   end
   
 #インタビュー新規作成(new.html.erbが投稿ページ。)
   def create
-   @interview = current_user.interview.build(interview_params)
+   @interview = current_user.build_interview(interview_params)
+     @user = current_user
      if @interview.save
        flash[:success] = "インタビューを公開しました"
-       redirect_to "interviews#show"
+       redirect_to interview_url
      else
        flash[:danger] = "インタビューの投稿に失敗しました"
-       redirect_to "users#show"
+       render "new"
      end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_143254) do
+ActiveRecord::Schema.define(version: 2019_09_16_123713) do
 
   create_table "interviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "answer_1"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_143254) do
     t.text "gakki"
     t.text "item"
     t.text "photo"
-    t.string "title"
+    t.text "title"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
