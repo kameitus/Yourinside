719e136d66dcd8212e23292294ac5212eb063610
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-21 06:37:25 +0000

    change logo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 before_action :require_user_logged_in, only:[:show,:update,:destroy]
 
-
 #検索一覧（条件ヒットに限る）
   def index
    @users = User.order(id: :desc).page(params[:page]).per(25)
