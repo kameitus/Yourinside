d2a6ee6171d6f1f2437bcceeec96523f413cf280
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-22 16:45:56 +0000

    praise unpraise button 実装

--- app/controllers/greats_controller.rb
+++ app/controllers/greats_controller.rb
@@ -5,13 +5,13 @@ class GreatsController < ApplicationController
     user = User.find(params[:great_id])
     current_user.praise(user)
     flash[:success] = "このインタビューに拍手を贈りました！"
-    redirect_to user
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     user = User.find(params[:great_id])
     current_user.unpraise(user)
     flash[:danger] = "このインタビューの拍手を取り消しました。"
-    redirect_to user
-  end
+    redirect_back(fallback_location: root_path)
+  end 
 end
